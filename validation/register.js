55cc0a9d288df5ebf5225cfd3f5d86382352ac1a
Selcendurmus <40351210+Selcendurmus@users.noreply.github.com>
2018-08-13 00:13:19 -0700

    react app login and registration

--- validation/register.js
+++ validation/register.js
@@ -34,12 +34,13 @@ module.exports = function validateRegisterInput(data){
     errors.password = 'Password field is required';
   }
 
-  if (!Validator.equals(data.password, data.password2)){
-    errors.password2 = 'Passwords must match';
-  }
 
   if (Validator.isEmpty(data.password2)){
     errors.password2 = 'Confirm password field is required';
+  }
+
+  if (!Validator.equals(data.password, data.password2)){
+    errors.password2 = 'Passwords must match';
   }
 
   return {
